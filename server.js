142b43c3bf3062774c32ed2ddab85f69f741be08
ConnieMarie <constance.marie.strahan@gmail.com>
2022-10-14 16:02:17 -0500

    format date plural and url

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
